020d2c7281b565312242aa573d2252de32af2edb
EricaIna <erica.rirura.4you@gmail.com>
2023-08-21 03:40:57 +0100

    main vs added

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ btn.onclick = () => {
 
 //copy right year
 document.getElementById("current-year").innerHTML = new Date().getFullYear();
+
+//video loop
+
+const video = document.getElementById("video");
+const videos = [
+  "img/mountain.mp4",
+  "img/ski.mp4",
+  "img/6.mp4",
+  "img/7.mp4",
+  // 他のビデオファイルを追加
+];
+let currentVideoIndex = 0;
+
+function playNextVideo() {
+  video.src = videos[currentVideoIndex];
+  video.play();
+  currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+}
+
+video.addEventListener("ended", playNextVideo);
+
+// play again
+video.play();
